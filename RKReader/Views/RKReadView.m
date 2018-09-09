b045fc50de961b59d1f82a61813a2f5a2ba84b4b
Odin <RZK@mba.local>
2018-09-09 12:50:02 +0800

    正确显示

--- RKReader/Views/RKReadView.m
+++ RKReader/Views/RKReadView.m
@@ -8,12 +8,6 @@
 
 #import "RKReadView.h"
 
-@interface RKReadView ()
-
-@property (nonatomic,assign) CTFrameRef frameRef;
-
-@end
-
 @implementation RKReadView
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -23,13 +17,6 @@
 		self.backgroundColor = [UIColor clearColor];
 	}
 	return self;
-}
-
-- (void)dealloc {
-	if (_frameRef) {
-		CFRelease(_frameRef);
-		_frameRef = nil;
-	}
 }
 
 #pragma mark - drawRect
@@ -45,36 +32,22 @@
     CGContextTranslateCTM(context, 0, self.bounds.size.height);
     CGContextScaleCTM(context, 1.0, -1.0);
     // 步骤 3
-//    CGMutablePathRef path = CGPathCreateMutable();
-//    CGPathAddRect(path, NULL, self.bounds);
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, self.bounds);
     // 步骤 4
-//    NSAttributedString *attString = [[NSAttributedString alloc] initWithString:self.content];
-//    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
-//    CTFrameRef frame = CTFramesetterCreateFrame(framesetter,
-//                             CFRangeMake(0, [attString length]), path, NULL);
+	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:self.content];
+	NSDictionary *attribute = [[RKUserConfiguration sharedInstance] parserAttribute];
+	[attributedString setAttributes:attribute range:NSMakeRange(0, self.content.length)];
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attributedString);
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter,
+                             CFRangeMake(0, [attributedString length]), path, NULL);
     // 步骤 5
-//    CTFrameDraw(frame, context);
-	CTFrameDraw(_frameRef, context);
+    CTFrameDraw(frame, context);
     // 步骤 6
-//    CFRelease(frame);
-//    CFRelease(path);
-//    CFRelease(framesetter);
+    CFRelease(frame);
+    CFRelease(path);
+    CFRelease(framesetter);
 }
 
-#pragma mark - setting
-- (void)setFrameRef:(CTFrameRef)frameRef {
-	if (_frameRef != frameRef) {
-		if (_frameRef) {
-			CFRelease(_frameRef);
-			_frameRef = nil;
-		}
-		_frameRef = frameRef;
-	}
-}
-
-- (void)setContent:(NSString *)content {
-	_content = content;
-	self.frameRef = [RKFileManager parserContent:self.content];
-}
 
 @end
